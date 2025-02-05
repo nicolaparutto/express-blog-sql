42c6248fdef6eee8f6d5e06a12b7a7de1c4fdd78
nicolaparutto <nicolaparutto2003@gmail.com>
2025-02-05 17:53:47 +0100

    style: correct code aesthetics

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -3,14 +3,11 @@ const connection = require('../data/postsDB.js');
 
 //Index (get)
 const index = (req, res) => {
-
    const sql = 'SELECT * FROM posts';
-
    connection.query(sql, (err, results) => {
       if (err) return res.status(500).json({ error: 'Errore durante il caricamento dei post' });
       res.json(results);
    })
-
 }
 
 //Show (get)
@@ -18,7 +15,6 @@ const show = (req, res) => {
    const id = req.params.id;
 
    const sql = 'SELECT * FROM posts WHERE id = ?';
-
    connection.query(sql, [id], (err, results) => {
       if (err) return res.status(500).json({ error: 'Errore durante il caricamento del post' });
       if (results.length === 0) return res.status(404).json({ error: 'Post non trovato' });
@@ -48,12 +44,10 @@ const destroy = (req, res) => {
    const id = req.params.id;
 
    const sql = 'DELETE FROM posts WHERE id = ?';
-
    connection.query(sql, [id], (err, results) => {
       if (err) return res.status(204).json({ error: 'Errore durante l\'elliminazione del post' });
       res.sendStatus(204);
    })
-
 }
 
 module.exports = {
