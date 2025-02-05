c5d3d317c2b30f80e63961f512c95d1c77f4948a
nicolaparutto <nicolaparutto2003@gmail.com>
2025-02-05 17:47:46 +0100

    feat: fourth milestone completed | show root can view a single post

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -16,7 +16,14 @@ const index = (req, res) => {
 //Show (get)
 const show = (req, res) => {
    const id = req.params.id;
-   res.send(`Visualizza il post ${id} del blog`)
+
+   const sql = 'SELECT * FROM posts WHERE id = ?';
+
+   connection.query(sql, [id], (err, results) => {
+      if (err) return res.status(500).json({ error: 'Errore durante il caricamento del post' });
+      if (results.length === 0) return res.status(404).json({ error: 'Post non trovato' });
+      res.json(results[0]);
+   })
 }
 
 //Store (post)
@@ -41,7 +48,7 @@ const destroy = (req, res) => {
    const id = req.params.id;
 
    const sql = 'DELETE FROM posts WHERE id = ?';
-   
+
    connection.query(sql, [id], (err, results) => {
       if (err) return res.status(204).json({ error: 'Errore durante l\'elliminazione del post' });
       res.sendStatus(204);
